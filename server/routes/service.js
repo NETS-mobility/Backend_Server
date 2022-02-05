c6810de0a1ac00fc3bc815dfa124d18107f72831
n3wzd <87504447+n3wzd@users.noreply.github.com>
2022-02-05 13:25:20 +0900

    Feature/ServiceLookup
    - 일부 파라미터 body->params 타입 변경
    - 설명문 주석 삭제 (swagger로 이동 예정)

--- server/routes/service.js
+++ server/routes/service.js
@@ -8,46 +8,10 @@ const pool2 = require('../modules/mysql2');
 const service_state = require('../config/service_state');
 
 
-/* ===== 서비스 목록 조회 =====
- *
- * 현재 진행중이거나 완료된 서비스 목록을 반환합니다. (픽업날짜 기준 오름차순 정렬된 상태)
- * UI에서 서비스 상태 판단하는 방법은 다음과 같습니다.
- * (service_state, needExtraPay) (X - 무관)
- *   예약 확정          (1, X)
- *   서비스 진행 중     (2, X)
- *   추가요금 결제      (3, 1)
- *   서비스 종료        (3, 0)
- *
- * === http-method ===
- * post
- *
- * === client-input ===
- * jwtToken: 사용자 정보 jwt토큰 (문자열)
- * listType: 반환할 서비스 상태 선택
- *   0 - 준비 or 진행중
- *   1 - 완료
- *
- * === server-return ===
- * serviceList: 진행중인 서비스 리스트 [{
- *   service_type: 서비스 종류 (문자열 - 네츠 무브, 네츠 휠체어, 네츠 휠체어 플러스),  
- *   service_id: 서비스 번호,
- *   pickup_time: 픽업 예정날짜&시간 (Date 객체),
- *   pickup_address: 픽업 주소,
- *   hos_name: 병원명,
- *   hos_arrival_time: 희망병원 도착시간,
- *   hos_care_time: 진료/검사 예약시간,
- *   hos_depart_time: 귀가 출발시간,
- *   netsmanager: 네츠매니저 이름,
- *   car_number: 차량번호,
- *   gowithumanager: 동행매니저 이름,
- *   service_state: 서비스 진행상태,
- *   needExtraPay: 추가결제 필요여부
- * }]
- *
-*/
-router.post('/serviceList', async function (req, res, next) {
+// ===== 서비스 목록 조회 =====
+router.post('/serviceList/:listType', async function (req, res, next) {
     const token = req.body.jwtToken;
-    const listType = req.body.listType;
+    const listType = req.params.listType;
 
     const token_res = await jwt.verify(token);
     if(token_res == jwt.TOKEN_EXPIRED) return res.status(401).send({ err : "만료된 토큰입니다." });
@@ -108,54 +72,9 @@ router.post('/serviceList', async function (req, res, next) {
 });
 
 
-/* ===== 서비스 상세보기 =====
- *
- * 서비스(예약) 1개의 정보를 반환합니다.
- * UI에서 서비스 상태 판단하는 방법은 '서비스 목록 조회' 서버페이지 것과 동일합니다.
- *
- * === http-method ===
- * post
- *
- * === client-input ===
- * service_id: 서비스 번호
- *
- * === server-return ===
- * // car: 차량정보 {
- * //   x: x좌표,
- * //   y: y좌표
- * // }
- * manager: 매니저 정보 {
- *   name: 이름,
- *   // job: 직업명,
- *   intro: 자기소개,
- *   tel: 전화번호,
- *   mention: 고객에게 전하는 말 (기본으로 설정된 내용)
- * }
- * // service_prog: 서비스 진도 (픽업완료, 병원도착, 귀가출발...)
- * // service_prog_nextime: 다음 서비스일정 시간
- * service: 서비스 정보 { 
- *   service_id: 서비스 번호,
- *   pickup_time: 픽업 예정날짜&시간 (Date 객체),
- *   pickup_address: 픽업 주소,
- *   hos_name: 병원명,
- *   hos_arrival_time: 희망병원 도착시간,
- *   hos_care_time: 진료/검사 예약시간,
- *   hos_depart_time: 귀가 출발시간,
- *   netsmanager: 네츠매니저 이름,
- *   car_number: 차량번호,
- *   gowithumanager: 동행매니저 이름,
- *   service_state: 서비스 진행상태,
- *   needExtraPay: 추가결제 필요여부
- * }
- * payment: 결제 정보 {
- *   charge: 요금,
- *   extraPay: 부가세
- * }
- *
- *
-*/
-router.post('/serviceDetail', async function (req, res, next) {
-    const service_id = req.body.service_id;
+// ===== 서비스 상세보기 =====
+router.post('/serviceDetail/:service_id', async function (req, res, next) {
+    const service_id = req.params.service_id;
 
     let err_custom = false;
     let err_msg = "";
