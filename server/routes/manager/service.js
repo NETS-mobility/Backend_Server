36350de589a6dab1e85908dbfcfac8a106a8080d
n3wzd <87504447+n3wzd@users.noreply.github.com>
2022-02-05 17:40:03 +0900

    Add Directory
    - 라우팅 디렉터리 앱 용도별로 세분화

---
+++ server/routes/manager/service.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+const pool = require('../../modules/mysql');
+const pool2 = require('../../modules/mysql2');
+
+
+// ===== 추가예정 =====
+
+
+module.exports = router;
