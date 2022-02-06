6dad4a21d0a55af3ee6772183db85df0abdf999d
YunseoKang <rkddbstj00@naver.com>
2022-02-06 23:01:00 +0900

    주석 추가

--- server/config/service_state.js
+++ server/config/service_state.js
@@ -2,8 +2,8 @@
 // 상태명 : reservation_state 번호
 
 module.exports = {
-    new : 0,                        // 준비 (확정X)
-    ready : 1,                      // 준비 (확정O)
+    new : 0,                        // 준비 (확정X)=예약 요청 이후 결제 대기
+    ready : 1,                      // 준비 (확정O)=예약 확정
     inProgress : 2,                 // 진행중
     complete : 3,                   // 완료
     cancelled : 4                   // 취소
