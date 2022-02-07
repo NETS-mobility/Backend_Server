833aad943a4e91aea64d67a1a81cd608fce4e318
n3wzd <87504447+n3wzd@users.noreply.github.com>
2022-02-08 08:54:19 +0900

    Feature/Client/MyPage
    - routes\client\mypage.js: 고객용 앱 마이페이지 추가
      - 비밀번호 변경
      - 개인정보 변경 (아이디 제외)
    - modules\jwt.js: 토큰 방식 변경
      - 토큰은 고객 주민번호를 저장하지 않음
      - 토큰 반환은 객체가 아닌 문자열로 바로 반환하게 변경
    - server.js: 마이페이지 라우팅 추가 및 라우팅 변수 이름변경

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 
 // 라우팅 js파일 목록
-const route_service = require('./routes/client/service');
+const route_client_service = require('./routes/client/service');
+const route_client_mypage = require('./routes/client/mypage');
 
 // npm 모듈 목록
 const cors = require("cors");
@@ -16,4 +17,5 @@ const port = 5000; // 포트 설정
 app.listen(port, () => console.log(`${port}`));
 
 // 서버별 도메인 설정 (app.use(bodyParser.json())코드 이후에 추가해야 bodyParser가 적용됨)
-app.use('/client/service', route_service);
+app.use('/client/service', route_client_service);
+app.use('/client/mypage', route_client_mypage);
