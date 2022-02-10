1a1b084fbec46691b1c1083b7b42b22dad504858
YunseoKang <rkddbstj00@naver.com>
2022-02-11 04:53:19 +0900

    twilio 관련 메세지 인증번호 전송 설정 저장

---
+++ server/modules/message.js
@@ -0,0 +1,24 @@
+// === SMS로 메세지 인증번호 전송 ===
+
+const accountSid = 'AC1a83473cd20eb2b01f96e14f23e9238a';
+const authToken = '533c9215965e253634b7cbb3218c28ed';
+const twilioPhone = '+18454425725';
+const twilio = require("twilio")(accountSid, authToken);
+
+module.exports = {
+    sendMessage: async (phone) => { // 메세지를 생성 및 전송, 올바른 인증번호 반환
+        const randomNumber = Math.floor(Math.random() * 1000000) + 1; // 6자리 난수 생성->인증번호
+        try {
+            const result = await twilio.messages.create({
+                body: `[NETS] 본인확인 인증번호 [${randomNumber}]를 입력해주세요.`,
+                from: twilioPhone,
+                to: phone,
+            });
+            if (result) return { randomNumber }; // 메세지 전송 성공, 인증번호 반환 
+            else return -1; // 메세지 전송 실패
+        }
+        catch (err) {
+            console.error("err : " + err);            
+        }
+    }
+};
