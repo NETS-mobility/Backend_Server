65afd97894e1ed5bae30155682920141b87fb415
YunseoKang <rkddbstj00@naver.com>
2022-03-01 05:38:20 +0900

    fix changePw error security processing about bcrypt salt

--- server/routes/manager/login.js
+++ server/routes/manager/login.js
@@ -7,21 +7,24 @@ const pool = require('../../modules/mysql');
 const pool2 = require('../../modules/mysql2');
 const message = require('../../modules/message');
 
+const bcrypt_option = require('../../config/bcrypt');
+const saltRounds = bcrypt_option.saltRounds;
+
 
 // ===== 로그인 =====
 router.post('', async function (req, res, next) {
     const { id, password } = req.body;
 
     const connection = await pool2.getConnection(async conn => conn);
-    try{
+    try {
         const sql = `SELECT netsmanager_password, netsmanager_name FROM netsmanager WHERE netsmanager_id=?;`;
-        const sql_result = await connection.query(sql, [id]);
-        const sql_data = sql_result[0];
+        const result = await connection.query(sql, [id]);
+        const sql_data = result[0];
         
-        if(sql_data.length == 0) return res.status(401).send({ msg : "아이디가 존재하지 않음" });
+        if (sql_data.length == 0) return res.status(401).send({ msg : "아이디가 존재하지 않음" });
         
         const validPassword = await bcrypt.compare(password, sql_data[0].netsmanager_password); // 복호화 비교
-        if(validPassword == 0) {
+        if (validPassword == 0) {
             res.status(401).send({ msg : "비밀번호가 일치하지 않음" });
         }
         else {
@@ -48,7 +51,7 @@ router.post('', async function (req, res, next) {
 router.post('/checkPhone', async function (req, res, next) {
     const phone = req.body.phone;
     const message_res = await message.sendMessage(phone); // 메세지 생성, 결과 얻음
-    if(message_res == -1) res.status(500).send({ err : "메세지 전송 실패"});
+    if (message_res == -1) res.status(500).send({ err : "메세지 전송 실패"});
     else res.status(200).send({ success : true, randomNumber : message_res }); // 인증번호 반환
 });
 
@@ -60,10 +63,10 @@ router.post('/findId', async function (req, res, next) {
     const connection = await pool2.getConnection(async conn => conn);
     try {
         const sql = `SELECT netsmanager_id FROM netsmanager WHERE netsmanager_phone=?;`;
-        const sql_result = await connection.query(sql, [phone]);
-        const sql_data = sql_result[0];
+        const result = await connection.query(sql, [phone]);
+        const sql_data = result[0];
 
-        if(sql_data.length == 0) res.status(401).send({ msg: "아이디가 존재하지 않음" });
+        if (sql_data.length == 0) res.status(401).send({ msg: "아이디가 존재하지 않음" });
         else res.status(200).send({ success : true, id : sql_data[0].netsmanager_id }); // 아이디 반환
     }
     catch (err) {
@@ -78,16 +81,23 @@ router.post('/findId', async function (req, res, next) {
 
 // ===== 비밀번호 변경 =====
 router.post('/changePw', async function (req, res, next) {
-    const {id, password} = req.body;
+    const { id, password } = req.body;
 
     const connection = await pool2.getConnection(async conn => conn);
-    try{
-        const salt = await bcrypt.genSalt(10);
+    try {
+        const salt = await bcrypt.genSalt(saltRounds);
         const hashed = await bcrypt.hash(password, salt); // 암호화
 
-        const sql = `UPDATE netsmanager SET netsmanager_password=? WHERE netsmanager_id=?;`;
-        await connection.query(sql, [hashed, id]);
-        res.status(200).send({ success : true });        
+        const sql1 = `SELECT netsmanager_id FROM netsmanager WHERE netsmanager_id=?;`;
+        const result1 = await connection.query(sql1, [id]);
+        const sql_data = result1[0];
+
+        if (sql_data.length == 0) res.status(401).send({ msg: "아이디가 존재하지 않음" });
+        else {
+            const sql2 = `UPDATE netsmanager SET netsmanager_password=? WHERE netsmanager_id=?;`;
+            await connection.query(sql2, [hashed, id]);
+            res.status(200).send({ success : true });
+        }
     }
     catch (err) {
         console.error("err : " + err);
