cc5c1316274cb572cb8ec133b53823b940da832c
YunseoKang <rkddbstj00@naver.com>
2022-03-01 05:41:13 +0900

    security processing about bcrypt salt

--- server/routes/client/register.js
+++ server/routes/client/register.js
@@ -8,6 +8,9 @@ const pool2 = require('../../modules/mysql2');
 const message = require('../../modules/message');
 const formatdate = require('../../modules/formatdate');
 
+const bcrypt_option = require('../../config/bcrypt');
+const saltRounds = bcrypt_option.saltRounds;
+
 
 // ===== 회원가입 - 아이디 중복확인 =====
 router.post('/checkDup', async function (req, res, next) {
@@ -16,10 +19,10 @@ router.post('/checkDup', async function (req, res, next) {
     const connection = await pool2.getConnection(async conn => conn);
     try {
         const sql = `SELECT user_id FROM user WHERE user_id=?;`;
-        const sql_result = await connection.query(sql, [id]);
-        const sql_data = sql_result[0];
+        const result = await connection.query(sql, [id]);
+        const sql_data = result[0];
 
-        if(sql_data.length == 0) res.status(200).send({ msg : "아이디 중복 안 됨, 회원가입 가능" });
+        if (sql_data.length == 0) res.status(200).send({ msg : "아이디 중복 안 됨, 회원가입 가능" });
         else res.status(401).send({ msg : "아이디 중복됨, 회원가입 불가능" });
     }
     catch (err) {
@@ -36,7 +39,7 @@ router.post('/checkDup', async function (req, res, next) {
 router.post('/checkPhone', async function (req, res, next) {
     const phone = req.body.phone;
     const message_res = await message.sendMessage(phone); // 메세지 생성, 결과 얻음
-    if(message_res == -1) res.status(500).send({ err : "메세지 전송 실패"});
+    if (message_res == -1) res.status(500).send({ err : "메세지 전송 실패"});
     else res.status(200).send({ success : true, randomNumber : message_res }); // 인증번호 반환
 });
 
@@ -47,7 +50,7 @@ router.post('', async function (req, res, next) {
 
     const connection = await pool2.getConnection(async conn => conn);
     try {
-        const salt = await bcrypt.genSalt(10);
+        const salt = await bcrypt.genSalt(saltRounds);
         const hashed = await bcrypt.hash(password, salt); // 암호화
         
         const sql = `INSERT INTO user (user_id, user_password, user_name, user_phone, user_join_date) VALUES (?,?,?,?,?);`;
