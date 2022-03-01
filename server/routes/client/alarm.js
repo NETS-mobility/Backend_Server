fe7342bc99fe0e0a7e0473fb4a6787095c49e130
jihong <ljihong0526@gmail.com>
2022-03-01 23:38:01 +0900

    client/alarm.js 파일 수정
    db변경사항에 맞게 user_id->user_number으로 변경

--- server/routes/client/alarm.js
+++ server/routes/client/alarm.js
@@ -1,47 +1,48 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { user } = require('../../config/database');
+const { user } = require("../../config/database");
 
-const jwt = require('../../modules/jwt');
-const pool2 = require('../../modules/mysql2');
+const jwt = require("../../modules/jwt");
+const pool2 = require("../../modules/mysql2");
 
 // ===== 알람 조회 =====
-router.post('', async function (req, res, next) {
-    const token = req.body.jwtToken;
+router.post("", async function (req, res, next) {
+  const token = req.body.jwtToken;
 
-    const token_res = await jwt.verify(token);
-    if(token_res == jwt.TOKEN_EXPIRED) return res.status(401).send({ err : "만료된 토큰입니다." });
-    if(token_res == jwt.TOKEN_INVALID) return res.status(401).send({ err : "유효하지 않은 토큰입니다." });
-    const user_id = token_res.id; // 이용자 id
-  
-    const connection = await pool2.getConnection(async conn => conn);
-    
-    try {
-        let param = [user_id];
+  const token_res = await jwt.verify(token);
+  if (token_res == jwt.TOKEN_EXPIRED)
+    return res.status(401).send({ err: "만료된 토큰입니다." });
+  if (token_res == jwt.TOKEN_INVALID)
+    return res.status(401).send({ err: "유효하지 않은 토큰입니다." });
+  const user_number = token_res.number; // 이용자 number
 
-        const sql = "select u.`user_name`, ca.`alarm_kind`, ca.`alarm_content`, r.`fixed_medical_time` " +
-                    "from `customer_alarm` as ca " +
-                    "left join user as u " +
-                    "on ca.`user_id` = u.`user_id` " +
-                    "left join reservation as r " +
-                    "on ca.`user_id` = r.`user_id` " +
-                    "where ca.`user_id` =? " +
-                    "order by ca.`alarm_id` desc;";
-            const result = await connection.query(sql, param);
-            const data = result[0];
-            connection.release();
+  const connection = await pool2.getConnection(async (conn) => conn);
 
-            res.send(data);
-        }
-    catch (err) {
-        console.error("err : " + err);
-        if(err == 0) res.status(401).send({ err : "잘못된 인자 전달" });
-        else res.status(500).send({ err : "서버 오류" });
-    }
-    finally{
-        connection.release();
-    }
+  try {
+    let param = [user_number];
+
+    const sql =
+      "select u.`user_name`, ca.`alarm_kind`, ca.`alarm_content`, r.`fixed_medical_time` " +
+      "from `customer_alarm` as ca " +
+      "left join user as u " +
+      "on ca.`user_number` = u.`user_number` " +
+      "left join reservation as r " +
+      "on ca.`user_number` = r.`user_number` " +
+      "where ca.`user_number` =? " +
+      "order by ca.`alarm_id` desc;";
+    const result = await connection.query(sql, param);
+    const data = result[0];
+    connection.release();
+
+    console.log(data);
+
+    res.send(data);
+  } catch (err) {
+    console.error("err : " + err);
+    if (err == 0) res.status(401).send({ err: "잘못된 인자 전달" });
+    else res.status(500).send({ err: "서버 오류" });
+  } finally {
+    connection.release();
+  }
 });
-
-
 module.exports = router;
