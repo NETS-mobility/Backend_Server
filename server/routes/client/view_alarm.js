511fa07075974daca47f75435b031f6a69ec5949
jihong <ljihong0526@gmail.com>
2022-03-16 08:24:24 +0900

    update view_alarm.js
    Modify sql statements according to DB changes

--- server/routes/client/view_alarm.js
+++ server/routes/client/view_alarm.js
@@ -22,14 +22,9 @@ router.post("/alarmList/", async function (req, res, next) {
     let param = [user_number];
 
     const sql =
-      "select u.`user_name`, ca.`alarm_kind`, ca.`alarm_content`, r.`fixed_medical_time` " +
-      "from `customer_alarm` as ca " +
-      "left join user as u " +
-      "on ca.`user_number` = u.`user_number` " +
-      "left join reservation as r " +
-      "on ca.`user_number` = r.`user_number` " +
-      "where u.`user_id` =? " +
-      "order by ca.`alarm_id` desc;";
+      "select *, u.`user_name` from `customer_alarm` as ca left join `user` as u on ca.`user_number` = u.`user_number` " +
+      "where u.user_id =? " +
+      "order by ca.`alarm_id` desc";
     const result = await connection.query(sql, param);
     const data = result[0];
     connection.release();
@@ -39,25 +34,6 @@ router.post("/alarmList/", async function (req, res, next) {
     console.error("err : " + err);
     if (err == 0) res.status(401).send({ err: "잘못된 인자 전달" });
     else res.status(500).send({ err: "서버 오류" });
-  } finally {
-    connection.release();
-  }
-});
-
-router.post("/alarmList/:alarm_id/", async function (req, res, next) {
-  const connection = await pool2.getConnection(async (conn) => conn);
-
-  try {
-    const alarm_id = req.params.alarm_id;
-
-    const sql = "select * from customer_alarm where alarm_id =?";
-    const result = await connection.query(sql, [alarm_id]);
-    const data = result[0];
-
-    res.send(data);
-  } catch (err) {
-    console.error("err : " + err);
-    res.status(500).send({ err: "서버 오류" });
   } finally {
     connection.release();
   }
--- server/routes/manager/view_alarm.js
+++ server/routes/manager/view_alarm.js
@@ -23,18 +23,9 @@ router.post("/alarmList/", async function (req, res, next) {
     let param = [manager_id];
 
     const sql =
-      "select m.`netsmanager_name`, ma.`alarm_kind`, ma.`alarm_content`, r.`reservation_id`, u.`user_name`, r.`reservation_submit_date`, r.`pickup_address`, r.`expect_pickup_time`, c.`car_number`, r.`gowithmanager_name` " +
-      "from manager_alarm as ma " +
-      "left join netsmanager as m " +
-      "on ma.`netsmanager_number` = m.`netsmanager_number` " +
-      "left join reservation as r " +
-      "on ma.`netsmanager_number` = r.`netsmanager_number` " +
-      "left join user as u " +
-      "on u.`user_number` = r.`user_number` " +
-      "left join car as c " +
-      "on c.`car_id` = r.`car_id` " +
-      "where m.`netsmanager_id` = ? " +
-      "order by ma.`alarm_id` desc;";
+      "select *, m.`netsmanager_name` from `manager_alarm` as ma left join `netsmanager` as m on ma.`netsmanager_number` = m.`netsmanager_number` " +
+      "where m.netsmanager_id =? " +
+      "order by ma.`alarm_id` desc";
     const result = await connection.query(sql, param);
     const data = result[0];
     connection.release();
@@ -44,26 +35,6 @@ router.post("/alarmList/", async function (req, res, next) {
     console.error("err : " + err);
     if (err == 0) res.status(401).send({ err: "잘못된 인자 전달" });
     else res.status(500).send({ err: "서버 오류" });
-  } finally {
-    connection.release();
-  }
-});
-
-// === 예약 상세 조회 ====
-router.post("/alarmList/:alarm_id/", async function (req, res, next) {
-  try {
-    const alarm_id = req.params.alarm_id;
-
-    const connection = await pool2.getConnection(async (conn) => conn);
-
-    const sql = "select * from manager_alarm where alarm_id =?";
-    const result = await connection.query(sql, [alarm_id]);
-    const data = result[0];
-
-    res.send(data);
-  } catch (err) {
-    console.error("err : " + err);
-    res.status(500).send({ err: "서버 오류" });
   } finally {
     connection.release();
   }
