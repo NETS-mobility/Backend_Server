c0455d6c16cb45dea9e8b9b0d519fcc5775c0851
n3wzd <87504447+n3wzd@users.noreply.github.com>
2022-03-16 16:09:59 +0900

    update insertDispatch
    - algorithm/algorithm/insertDispatch: DB 호환 업데이트

--- server/algorithm/algorithm/insertDispatch.js
+++ server/algorithm/algorithm/insertDispatch.js
@@ -25,6 +25,7 @@
 
 
 const pool2 = require("../util/mysql2");
+const rev_id = "200209000000"; // 예약 아이디 (수정 필요)
 
 const InsertDispatch = async (dispatchResult, revData, direction, is3) => {
   let success = true;
@@ -35,21 +36,17 @@ const InsertDispatch = async (dispatchResult, revData, direction, is3) => {
     for(let i = 0; i < dispatchResult.length; i++)
     {
       const dire = direction[i];
-      const rev_id = revData.rev_id;
       const car_id = dispatchResult[i].dispatch[0].car_id;
       const adr_start = (dire == 1) ? revData.pickup : revData.hos;
       const adr_end = (dire == 1) ? revData.hos : revData.drop;
+      const edist = dispatchResult[i].expect_move_distance;
+      const etime = dispatchResult[i].expect_move_time 
       let time_start = dispatchResult[i].expect_pickup_time;
       let time_end = dispatchResult[i].expect_terminate_service_time;
-      let prev_dep_time = dispatchResult[i].dispatch[0].prevDepartureTime;
       if(is3)
       {
         if(dire == 1) time_end = revData.rev_date + " " + revData.old_hos_arr_time; // case 3일 경우, 병원 도착 시간을 기준으로 2개로 분할
-        if(dire == 2) 
-        {
-          time_start = revData.rev_date + " " + revData.old_hos_arr_time;
-          prev_dep_time = time_start; // case 3 후반 배차의 출발 시각은 동행 시작 시간으로 설정
-        }
+        if(dire == 2) time_start = revData.rev_date + " " + revData.old_hos_arr_time; // case 3 후반 배차의 출발 시각은 동행 시작 시간으로 설정
       }
 
        // 매니저 매칭: 기본적으로 차량의 운전자로 배정
@@ -81,13 +78,11 @@ const InsertDispatch = async (dispatchResult, revData, direction, is3) => {
 
       const time_startd = new Date(time_start);
       const time_endd = new Date(time_end);
-      const prev_dep_timed = new Date(prev_dep_time);
       const time_start2 = time_startd.getFullYear() + "-" + (time_startd.getMonth()+1) + "-" + time_startd.getDate() + " " + time_startd.getHours() + ":" + time_startd.getMinutes();
       const time_end2 = time_endd.getFullYear() + "-" + (time_endd.getMonth()+1) + "-" + time_endd.getDate() + " " + time_endd.getHours() + ":" + time_endd.getMinutes();
-      const prev_dep_time2 = prev_dep_timed.getFullYear() + "-" + (prev_dep_timed.getMonth()+1) + "-" + prev_dep_timed.getDate() + " " + prev_dep_timed.getHours() + ":" + prev_dep_timed.getMinutes();
 
-      const sql = "insert into `car_dispatch`(`reservation_id`,`car_id`,`netsmanager_number`,`departure_address`,`destination_address`,`expect_car_pickup_time`,`expect_car_terminate_service_time`) values (?,?,?,?,?,?,?);"
-      await connection.query(sql, ["200209000000", car_id, manager_number, adr_start, adr_end, time_start2, time_end2, prev_dep_time2]);
+      const sql = "insert into `car_dispatch`(`reservation_id`,`car_id`,`netsmanager_number`,`car_move_direction_id`,`departure_address`,`destination_address`,`expect_car_pickup_time`,`expect_car_terminate_service_time`,`expect_move_distance`,`expect_move_time`) values (?,?,?,?,?,?,?,?,?,?);"
+      await connection.query(sql, [rev_id, car_id, manager_number, dire, adr_start, adr_end, time_start2, time_end2, edist, etime]);
     }
 
     await connection.commit() // 완료시 커밋
