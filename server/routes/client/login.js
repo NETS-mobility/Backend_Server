f9d56fad4c7565631956b27705730d9a12f57346
ssu00 <rachel4535@naver.com>
2022-03-18 21:02:28 +0900

    🐛 fix bug for jwt

--- server/routes/client/login.js
+++ server/routes/client/login.js
@@ -1,111 +1,108 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
-const jwt = require('../../modules/jwt');
-const pool = require('../../modules/mysql');
-const pool2 = require('../../modules/mysql2');
-const message = require('../../modules/message');
+const jwt = require("../../modules/jwt");
+const pool = require("../../modules/mysql");
+const pool2 = require("../../modules/mysql2");
+const message = require("../../modules/message");
 
-const bcrypt_option = require('../../config/bcrypt');
+const bcrypt_option = require("../../config/bcrypt");
 const saltRounds = bcrypt_option.saltRounds;
 
-
 // ===== 로그인 =====
-router.post('', async function (req, res, next) {
-    const { id, password } = req.body;
+router.post("", async function (req, res, next) {
+  const { id, password } = req.body;
 
-    const connection = await pool2.getConnection(async conn => conn);
-    try {
-        const sql = `SELECT user_password, user_name FROM user WHERE user_id=?;`;
-        const result = await connection.query(sql, [id]);
-        const sql_data = result[0];
-        
-        if (sql_data.length == 0) return res.status(401).send({ msg : "아이디가 존재하지 않음" });
-        
-        const validPassword = await bcrypt.compare(password, sql_data[0].user_password); // 복호화 비교
-        if (validPassword == 0) {
-            res.status(401).send({ msg : "비밀번호가 일치하지 않음" });
-        }
-        else {
-            const payload = { // 고객 정보
-                id : id,
-                name : sql_data[0].user_name,
-            };
+  const connection = await pool2.getConnection(async (conn) => conn);
+  try {
+    const sql = `SELECT user_password, user_name,user_number FROM user WHERE user_id=?;`;
+    const result = await connection.query(sql, [id]);
+    const sql_data = result[0];
 
-            const token_res = await jwt.sign(payload); // 토큰 생성
-            res.status(200).send({ success : true, token : token_res });
-        }
+    if (sql_data.length == 0)
+      return res.status(401).send({ msg: "아이디가 존재하지 않음" });
+
+    const validPassword = await bcrypt.compare(
+      password,
+      sql_data[0].user_password
+    ); // 복호화 비교
+    if (validPassword == 0) {
+      res.status(401).send({ msg: "비밀번호가 일치하지 않음" });
+    } else {
+      const payload = {
+        // 고객 정보
+        id: id,
+        name: sql_data[0].user_name,
+        num: sql_data[0].user_number,
+      };
+
+      const token_res = await jwt.sign(payload); // 토큰 생성
+      res.status(200).send({ success: true, token: token_res });
     }
-    catch (err) {
-        console.error("err : " + err);
-        res.status(500).send({ err : "서버 오류" });
-    }
-    finally {
-        connection.release();
-    }
+  } catch (err) {
+    console.error("err : " + err);
+    res.status(500).send({ err: "서버 오류" });
+  } finally {
+    connection.release();
+  }
 });
-
 
 // ===== 휴대폰 인증-인증번호 반환 =====
-router.post('/checkPhone', async function (req, res, next) {
-    const phone = req.body.phone;
-    const message_res = await message.sendMessage(phone); // 메세지 생성, 결과 얻음
-    if (message_res == -1) res.status(500).send({ err : "메세지 전송 실패"});
-    else res.status(200).send({ success : true, randomNumber : message_res }); // 인증번호 반환
+router.post("/checkPhone", async function (req, res, next) {
+  const phone = req.body.phone;
+  const message_res = await message.sendMessage(phone); // 메세지 생성, 결과 얻음
+  if (message_res == -1) res.status(500).send({ err: "메세지 전송 실패" });
+  else res.status(200).send({ success: true, randomNumber: message_res }); // 인증번호 반환
 });
-
 
 // ===== 아이디 찾기 =====
-router.post('/findId', async function (req, res, next) {
-    const phone = req.body.phone;
-    
-    const connection = await pool2.getConnection(async conn => conn);
-    try {
-        const sql = `SELECT user_id FROM user WHERE user_phone=?;`;
-        const result = await connection.query(sql, [phone]);
-        const sql_data = result[0];
+router.post("/findId", async function (req, res, next) {
+  const phone = req.body.phone;
 
-        if (sql_data.length == 0) res.status(401).send({ msg: "아이디가 존재하지 않음" });
-        else res.status(200).send({ success : true, id : sql_data[0].user_id }); // 아이디 반환
-    }
-    catch (err) {
-        console.error("err : " + err);
-        res.status(500).send({ err : "서버 오류" });
-    }
-    finally {
-        connection.release();
-    }
+  const connection = await pool2.getConnection(async (conn) => conn);
+  try {
+    const sql = `SELECT user_id FROM user WHERE user_phone=?;`;
+    const result = await connection.query(sql, [phone]);
+    const sql_data = result[0];
+
+    if (sql_data.length == 0)
+      res.status(401).send({ msg: "아이디가 존재하지 않음" });
+    else res.status(200).send({ success: true, id: sql_data[0].user_id }); // 아이디 반환
+  } catch (err) {
+    console.error("err : " + err);
+    res.status(500).send({ err: "서버 오류" });
+  } finally {
+    connection.release();
+  }
 });
 
-
 // ===== 비밀번호 변경 =====
-router.post('/changePw', async function (req, res, next) {
-    const { id, password } = req.body;
+router.post("/changePw", async function (req, res, next) {
+  const { id, password } = req.body;
 
-    const connection = await pool2.getConnection(async conn => conn);
-    try {
-        const salt = await bcrypt.genSalt(saltRounds);
-        const hashed = await bcrypt.hash(password, salt); // 암호화
+  const connection = await pool2.getConnection(async (conn) => conn);
+  try {
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hashed = await bcrypt.hash(password, salt); // 암호화
 
-        const sql1 = `SELECT user_id FROM user WHERE user_id=?;`;
-        const result1 = await connection.query(sql1, [id]);
-        const sql_data = result1[0];
+    const sql1 = `SELECT user_id FROM user WHERE user_id=?;`;
+    const result1 = await connection.query(sql1, [id]);
+    const sql_data = result1[0];
 
-        if (sql_data.length == 0) res.status(401).send({ msg: "아이디가 존재하지 않음" });
-        else {
-            const sql2 = `UPDATE user SET user_password=? WHERE user_id=?;`;
-            await connection.query(sql2, [hashed, id]);
-            res.status(200).send({ success : true });
-        }
+    if (sql_data.length == 0)
+      res.status(401).send({ msg: "아이디가 존재하지 않음" });
+    else {
+      const sql2 = `UPDATE user SET user_password=? WHERE user_id=?;`;
+      await connection.query(sql2, [hashed, id]);
+      res.status(200).send({ success: true });
     }
-    catch (err) {
-        console.error("err : " + err);
-        res.status(500).send({ err : "서버 오류" });
-    }
-    finally {
-        connection.release();
-    }
+  } catch (err) {
+    console.error("err : " + err);
+    res.status(500).send({ err: "서버 오류" });
+  } finally {
+    connection.release();
+  }
 });
 
 module.exports = router;
--- server/routes/client/mypage.js
+++ server/routes/client/mypage.js
@@ -1,158 +1,159 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const jwt = require('../../modules/jwt');
-const pool = require('../../modules/mysql');
-const pool2 = require('../../modules/mysql2');
-const bcrypt = require('bcryptjs');
+const jwt = require("../../modules/jwt");
+const pool = require("../../modules/mysql");
+const pool2 = require("../../modules/mysql2");
+const bcrypt = require("bcryptjs");
 
-const bcrypt_option = require('../../config/bcrypt');
+const bcrypt_option = require("../../config/bcrypt");
 const saltRounds = bcrypt_option.saltRounds;
 
-
 // ===== 마이페이지 입장 =====
-router.post('', async function (req, res, next) {
-    const token = req.body.jwtToken;
+router.post("", async function (req, res, next) {
+  const token = req.body.jwtToken;
 
-    const token_res = await jwt.verify(token);
-    if(token_res == jwt.TOKEN_EXPIRED) return res.status(401).send({ err : "만료된 토큰입니다." });
-    if(token_res == jwt.TOKEN_INVALID) return res.status(401).send({ err : "유효하지 않은 토큰입니다." });
-    const user_id = token_res.id;
+  console.log("token==", token);
+  const token_res = await jwt.verify(token);
+  console.log("token_res==========", token_res);
+  if (token_res == jwt.TOKEN_EXPIRED)
+    return res.status(401).send({ err: "만료된 토큰입니다." });
+  if (token_res == jwt.TOKEN_INVALID)
+    return res.status(401).send({ err: "유효하지 않은 토큰입니다." });
+  const user_num = token_res.num;
 
-    const connection = await pool2.getConnection(async conn => conn);
-    try {
-        const sql = "select `user_id` as `id`, `user_name` as `name`, `user_phone` as `phone` from `user` where `user_number`=?;";
-        const sql_result = await connection.query(sql, [user_id]);
-        const sql_data = sql_result[0];
-        if(sql_data.length == 0) throw err = 0;
-        res.send(sql_data[0]);
-    }
-    catch (err) {
-        console.error("err : " + err);
-        if(err == 0) res.status(400).send({ err : "회원정보가 존재하지 않습니다." });
-        else res.status(500).send({ err : "서버 오류" });
-    }
-    finally {
-        connection.release();
-    }
+  const connection = await pool2.getConnection(async (conn) => conn);
+  try {
+    const sql =
+      "select `user_id` as `id`, `user_name` as `name`, `user_phone` as `phone` from `user` where `user_number`=?;";
+    const sql_result = await connection.query(sql, [user_num]);
+    const sql_data = sql_result[0];
+    if (sql_data.length == 0) throw (err = 0);
+    res.send(sql_data[0]);
+  } catch (err) {
+    console.error("err : " + err);
+    if (err == 0)
+      res.status(400).send({ err: "회원정보가 존재하지 않습니다." });
+    else res.status(500).send({ err: "서버 오류" });
+  } finally {
+    connection.release();
+  }
 });
-
 
 // ===== 마이페이지 개인정보 변경-아이디 중복확인 =====
-router.post('/changeInfo/checkDup', async function (req, res, next) {
-    const token = req.body.jwtToken;
-    const user_newId = req.body.user_newId;
+router.post("/changeInfo/checkDup", async function (req, res, next) {
+  const token = req.body.jwtToken;
+  const user_newId = req.body.user_newId;
 
-    const token_res = await jwt.verify(token);
-    if(token_res == jwt.TOKEN_EXPIRED) return res.status(401).send({ err : "만료된 토큰입니다." });
-    if(token_res == jwt.TOKEN_INVALID) return res.status(401).send({ err : "유효하지 않은 토큰입니다." });
-    const user_number = token_res.id;
+  const token_res = await jwt.verify(token);
+  if (token_res == jwt.TOKEN_EXPIRED)
+    return res.status(401).send({ err: "만료된 토큰입니다." });
+  if (token_res == jwt.TOKEN_INVALID)
+    return res.status(401).send({ err: "유효하지 않은 토큰입니다." });
+  const user_number = token_res.id;
 
-    const connection = await pool2.getConnection(async conn => conn);
-    try {
-        const sql = "select `user_id` as `id` from `user` where `user_id`=? and `user_number`!=?;";
-        const sql_result = await connection.query(sql, [user_newId, user_number]);
-        const sql_data = sql_result[0];
+  const connection = await pool2.getConnection(async (conn) => conn);
+  try {
+    const sql =
+      "select `user_id` as `id` from `user` where `user_id`=? and `user_number`!=?;";
+    const sql_result = await connection.query(sql, [user_newId, user_number]);
+    const sql_data = sql_result[0];
 
-        if(sql_data.length == 0) res.send({isDup: false}); 
-        else res.send({isDup: true});
-    }
-    catch (err) {
-        console.error("err : " + err);
-        res.status(500).send({ err : "서버 오류" });
-    }
-    finally {
-        connection.release();
-    }
+    if (sql_data.length == 0) res.send({ isDup: false });
+    else res.send({ isDup: true });
+  } catch (err) {
+    console.error("err : " + err);
+    res.status(500).send({ err: "서버 오류" });
+  } finally {
+    connection.release();
+  }
 });
-
 
 // ===== 마이페이지 개인정보 변경 =====
-router.post('/changeInfo', async function (req, res, next) {
-    const token = req.body.jwtToken;
-    const user_name = req.body.user_name;
-    const user_newId = req.body.user_newId;
-    const user_phone = req.body.user_phone;
+router.post("/changeInfo", async function (req, res, next) {
+  const token = req.body.jwtToken;
+  const user_name = req.body.user_name;
+  const user_newId = req.body.user_newId;
+  const user_phone = req.body.user_phone;
 
-    const token_res = await jwt.verify(token);
-    if(token_res == jwt.TOKEN_EXPIRED) return res.status(401).send({ err : "만료된 토큰입니다." });
-    if(token_res == jwt.TOKEN_INVALID) return res.status(401).send({ err : "유효하지 않은 토큰입니다." });
-    const user_id = token_res.id; // 이용자 id
+  const token_res = await jwt.verify(token);
+  if (token_res == jwt.TOKEN_EXPIRED)
+    return res.status(401).send({ err: "만료된 토큰입니다." });
+  if (token_res == jwt.TOKEN_INVALID)
+    return res.status(401).send({ err: "유효하지 않은 토큰입니다." });
+  const user_id = token_res.id; // 이용자 id
 
-    const connection = await pool2.getConnection(async conn => conn);
-    try {
-        const sql = "update `user` set `user_id`=?, `user_name`=?, `user_phone`=? where `user_number`=?;";
-        await connection.query(sql, [user_newId, user_name, user_phone, user_id]);
-        res.status(200).send();
-    }
-    catch (err) {
-        console.error("err : " + err);
-        res.status(500).send({ err : "서버 오류" });
-    }
-    finally {
-        connection.release();
-    }
+  const connection = await pool2.getConnection(async (conn) => conn);
+  try {
+    const sql =
+      "update `user` set `user_id`=?, `user_name`=?, `user_phone`=? where `user_number`=?;";
+    await connection.query(sql, [user_newId, user_name, user_phone, user_id]);
+    res.status(200).send();
+  } catch (err) {
+    console.error("err : " + err);
+    res.status(500).send({ err: "서버 오류" });
+  } finally {
+    connection.release();
+  }
 });
-
 
 // ===== 마이페이지 비밀번호 변경 - 아이디 확인 =====
-router.post('/changePw/checkId', async function (req, res, next) {
-    const token = req.body.jwtToken;
-    const user_id = req.body.user_id;
+router.post("/changePw/checkId", async function (req, res, next) {
+  const token = req.body.jwtToken;
+  const user_id = req.body.user_id;
 
-    const token_res = await jwt.verify(token);
-    if(token_res == jwt.TOKEN_EXPIRED) return res.status(401).send({ err : "만료된 토큰입니다." });
-    if(token_res == jwt.TOKEN_INVALID) return res.status(401).send({ err : "유효하지 않은 토큰입니다." });
-    const user_number = token_res.id;
+  const token_res = await jwt.verify(token);
+  if (token_res == jwt.TOKEN_EXPIRED)
+    return res.status(401).send({ err: "만료된 토큰입니다." });
+  if (token_res == jwt.TOKEN_INVALID)
+    return res.status(401).send({ err: "유효하지 않은 토큰입니다." });
+  const user_number = token_res.id;
 
-    const connection = await pool2.getConnection(async conn => conn);
-    try {
-        const sql = "select `user_id` as `id` from `user` where `user_number`=?;";
-        const sql_result = await connection.query(sql, [user_number]);
-        const sql_data = sql_result[0];
-        if(sql_data.length == 0) throw err = 0;
+  const connection = await pool2.getConnection(async (conn) => conn);
+  try {
+    const sql = "select `user_id` as `id` from `user` where `user_number`=?;";
+    const sql_result = await connection.query(sql, [user_number]);
+    const sql_data = sql_result[0];
+    if (sql_data.length == 0) throw (err = 0);
 
-        if(sql_data[0].id == user_id) res.send({ ok : true });
-        else res.send({ ok : false });
-    }
-    catch (err) {
-        console.error("err : " + err);
-        if(err == 0) res.status(400).send({ err : "회원정보가 존재하지 않습니다." });
-        else res.status(500).send({ err : "서버 오류" });
-    }
-    finally {
-        connection.release();
-    }
+    if (sql_data[0].id == user_id) res.send({ ok: true });
+    else res.send({ ok: false });
+  } catch (err) {
+    console.error("err : " + err);
+    if (err == 0)
+      res.status(400).send({ err: "회원정보가 존재하지 않습니다." });
+    else res.status(500).send({ err: "서버 오류" });
+  } finally {
+    connection.release();
+  }
 });
-
 
 // ===== 마이페이지 비밀번호 변경 - 비밀번호 변경 =====
-router.post('/changePw', async function (req, res, next) {
-    const token = req.body.jwtToken;
-    const user_pw = req.body.user_pw;
-    console.log(req.body);
+router.post("/changePw", async function (req, res, next) {
+  const token = req.body.jwtToken;
+  const user_pw = req.body.user_pw;
+  console.log(req.body);
 
-    const token_res = await jwt.verify(token);
-    if(token_res == jwt.TOKEN_EXPIRED) return res.status(401).send({ err : "만료된 토큰입니다." });
-    if(token_res == jwt.TOKEN_INVALID) return res.status(401).send({ err : "유효하지 않은 토큰입니다." });
-    const user_id = token_res.id; // 이용자 id
+  const token_res = await jwt.verify(token);
+  if (token_res == jwt.TOKEN_EXPIRED)
+    return res.status(401).send({ err: "만료된 토큰입니다." });
+  if (token_res == jwt.TOKEN_INVALID)
+    return res.status(401).send({ err: "유효하지 않은 토큰입니다." });
+  const user_id = token_res.id; // 이용자 id
 
-    const connection = await pool2.getConnection(async conn => conn);
-    try {
-        const user_hashedPw = await bcrypt.hash(user_pw, saltRounds);
-        const sql = "update `user` set `user_password`=? where `user_number`=?;";
-        await connection.query(sql, [user_hashedPw, user_id]);
-        res.status(200).send();
-    }
-    catch (err) {
-        console.error("err : " + err);
-        res.status(500).send({ err : "서버 오류" });
-    }
-    finally {
-        connection.release();
-    }
+  const connection = await pool2.getConnection(async (conn) => conn);
+  try {
+    const user_hashedPw = await bcrypt.hash(user_pw, saltRounds);
+    const sql = "update `user` set `user_password`=? where `user_number`=?;";
+    await connection.query(sql, [user_hashedPw, user_id]);
+    res.status(200).send();
+  } catch (err) {
+    console.error("err : " + err);
+    res.status(500).send({ err: "서버 오류" });
+  } finally {
+    connection.release();
+  }
 });
-
 
 // ===== 마이페이지 FAQ =====
 // ===== 마이페이지 공지사항 =====
