a3131edbf317405b5de473d1dfb8e264bf27593c
YunseoKang <rkddbstj00@naver.com>
2022-03-20 02:32:20 +0900

    fix a bug for client, manager login

--- server/routes/manager/login.js
+++ server/routes/manager/login.js
@@ -17,9 +17,9 @@ router.post("", async function (req, res, next) {
 
   const connection = await pool2.getConnection(async (conn) => conn);
   try {
-    const sql = `SELECT netsmanager_password, netsmanager_name, is_check_phone FROM netsmanager WHERE netsmanager_id=?;`;
-    const result = await connection.query(sql, [id]);
-    const sql_data = result[0];
+    const sql1 = `SELECT netsmanager_password, netsmanager_name, is_check_phone FROM netsmanager WHERE netsmanager_id=?;`;
+    const result1 = await connection.query(sql1, [id]);
+    const sql_data = result1[0];
 
     if (sql_data.length == 0)
       return res.status(401).send({ msg: "아이디가 존재하지 않음" });
@@ -52,9 +52,8 @@ router.post("", async function (req, res, next) {
         });
       }
       // 매니저의 디바이스 토큰값 갱신(push 알림에 사용)
-      sql =
-        "update netsmanager set `netsmanager_device_token` =? where `netsmanager_id` =?;";
-      result = await connection.query(sql, [device_token]);
+      const sql2 = "update netsmanager set `netsmanager_device_token` =? where `netsmanager_id` =?;";
+      const result2 = await connection.query(sql2, [device_token]);
     }
   } catch (err) {
     console.error("err : " + err);
