ea4c86934385d11a702cecf6b9fac15324784816
이지홍 <ljihong0526@gmail.com>
2022-03-21 03:58:50 +0900

    Update database.js
    fix error

--- server/config/database.js
+++ server/config/database.js
@@ -6,6 +6,6 @@ module.exports = {
   user: process.env.DB_USER,
   password: process.env.DB_PSWORD, // gcp 테스트 시
   //    password: '1234',                    // localhost 테스트 시
-  database: process.env.DATABASE,
+  database: process.env.DB_DATABASE,
   multipleStatements: true,
 };
