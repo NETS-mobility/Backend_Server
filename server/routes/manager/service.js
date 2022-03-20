72e8371c8651b8fcc43acc5fde045f88fa7fd4e2
n3wzd <87504447+n3wzd@users.noreply.github.com>
2022-03-20 14:32:58 +0900

    add user phone
    /manager/service: 고객 전화번호 추가

--- server/routes/manager/service.js
+++ server/routes/manager/service.js
@@ -33,7 +33,7 @@ router.post("/serviceList/:listType", async function (req, res, next) {
     let sql1 =
       "select S.`service_kind` as `service_type`, cast(R.`reservation_id` as char) as `service_id`, `expect_pickup_time` as `pickup_time`, `hope_reservation_date` as `rev_date`, `pickup_address`, " +
       "`hospital_address` as `hos_address`, `hope_hospital_arrival_time` as `hos_arrival_time`, `fixed_medical_time` as `hos_care_time`, `hope_hospital_departure_time` as `hos_depart_time`, " +
-      "U.`user_name` as `user_name`, `gowithmanager_name` as `gowithumanager`, `reservation_state_id` as `reservation_state` " +
+      "U.`user_name` as `user_name`, U.`user_phone` as `user_phone`, `gowithmanager_name` as `gowithumanager`, `reservation_state_id` as `reservation_state` " +
       "from `car_dispatch` as C, `reservation` as R, `service_info` as S, `user` as U, `netsmanager` as NM " +
       "where C.`netsmanager_number`=? and C.`reservation_id`=R.`reservation_id` and R.`service_kind_id`=S.`service_kind_id` and C.`netsmanager_number`=NM.`netsmanager_number` and R.`user_number`=U.`user_number` ";
 
