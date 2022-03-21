8f15dfb4f41223c4ae0c06fe4f8d652538eae2eb
ssu00 <rachel4535@naver.com>
2022-03-21 20:59:08 +0900

    🧪 add for test

--- server/routes/client/service.js
+++ server/routes/client/service.js
@@ -149,6 +149,12 @@ router.post("/serviceDetail/:service_id", async function (req, res, next) {
       "select * from `payment` where `payment_type`=2 and `payment_state_id`=1 and `reservation_id`=?;";
     const sqlmr = await connection.query(sqlm, [service_id]);
     const isNeedExtraPay = sqlmr[0].length > 0;
+    console.log("sqlmr==", sqlmr);
+    console.log("sqlmr[0]==", sqlmr[0]);
+    console.log("sqlmr[0][0]==", sqlmr[0][0]);
+    console.log("sqlmr[0][1]==", sqlmr[0][1]);
+    console.log("sqlmr[0][0].cost==", sqlmr[0][0].cost);
+    console.log("sqlmr[0][1].cost==", sqlmr[0][1].cost);
     data_service[0].reservation_state = rev_state_msg(
       data_service[0].reservation_state,
       isNeedExtraPay
