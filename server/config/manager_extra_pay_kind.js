9c2d492d533ecd01d31cfe29593a7902756af961
YunseoKang <rkddbstj00@naver.com>
2022-03-21 23:08:25 +0900

    add delimiter

--- server/config/manager_extra_pay_kind.js
+++ server/config/manager_extra_pay_kind.js
@@ -5,7 +5,7 @@ module.exports = {
     night : 2,                      // 야간근로수당
     saturday : 3,                   // 휴무일근로수당(토요일)
     holiday : 4,                    // 휴일근로수당(빨간날)
-    night_overtime : 5              // 야간근무+연장근무
-    holiday_overtime : 6            // 휴일근무+연장근무
+    night_overtime : 5,             // 야간근무+연장근무
+    holiday_overtime : 6,           // 휴일근무+연장근무
     holiday_night_overtime : 7      // 휴일근무+연장근무+야간근무
 }
