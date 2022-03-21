8eb3a2dd8997703c73512174039cd435ac82d0f2
ssu00 <rachel4535@naver.com>
2022-03-21 11:25:58 +0900

    🧪 add for test

--- server/routes/manager/service.js
+++ server/routes/manager/service.js
@@ -218,6 +218,7 @@ router.post(
   "/serviceDetail/:service_id/submitDoc",
   upload(uplPath.customer_document).single("file"),
   async function (req, res, next) {
+    console.log("req==", req);
     const file = req.file;
     if (file === undefined)
       return res.status(400).send({ err: "파일이 업로드되지 않았습니다." });
