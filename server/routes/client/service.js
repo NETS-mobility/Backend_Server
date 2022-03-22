40dcf78645d7621a9bf91da4c640545cb7b6589f
ssu00 <rachel4535@naver.com>
2022-03-22 21:54:28 +0900

    🐛 fix a bug for service List

--- server/routes/client/service.js
+++ server/routes/client/service.js
@@ -26,8 +26,7 @@ router.post("/serviceList/:listType", async function (req, res, next) {
     return res.status(401).send({ err: "만료된 토큰입니다." });
   if (token_res == jwt.TOKEN_INVALID)
     return res.status(401).send({ err: "유효하지 않은 토큰입니다." });
-  const user_num = 1;
-  token_res.num;
+  const user_num = token_res.num;
   console.log("user_num==", user_num);
 
   const connection = await pool2.getConnection(async (conn) => conn);
