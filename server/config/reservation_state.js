7bb00eda58898e04625170f33c06e68e66f383a4
이지홍 <ljihong0526@gmail.com>
2022-03-26 17:27:03 +0900

    Revert "Fix/state"

--- server/config/reservation_state.js
+++ server/config/reservation_state.js
@@ -2,10 +2,9 @@
 // 상태명 : reservation_state 번호
 
 module.exports = {
-  new: 0, // 준비 - 예약 요청(결제 대기)
-  ready: 1, // 준비 - 예약 확정(결제 완료)
-  inProgress: 2, // 서비스 진행 중
-  complete: 3, // 서비스 완료
-  waitCancelled: 4, // 예약 취소 대기
-  Cancelled: 5, // 예약 취소 완료
-};
+  new: 0,                        // 준비 - 예약 요청(결제 대기)
+  ready: 1,                      // 준비 - 예약 확정(결제 완료)
+  inProgress: 2,                 // 서비스 진행 중
+  complete: 3,                   // 서비스 완료
+  cancelled: 4                   // 예약 취소
+}
