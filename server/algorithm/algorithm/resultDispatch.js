84502548dfb972d7cf981c6c66267c326a9ca529
n3wzd <87504447+n3wzd@users.noreply.github.com>
2022-03-30 16:23:18 +0900

    consider manager_possible
    배차에서 매니저 배차가능여부 판정 추가

--- server/algorithm/algorithm/resultDispatch.js
+++ server/algorithm/algorithm/resultDispatch.js
@@ -3,7 +3,7 @@
 // === 배차 선택 ===
 // 픽업장소 출발 => 픽업에서 가장 가까운 곳에 있는 최적 차량 선택
 // 병원에서 출발 => 병원과 가장 가까운 곳에 있는 최적 차량 선택
-// prevDepartureTime이 가장 큰 순서대로 정렬되어 있으므로, 최적 차량 = dispatch[0]
+// prevDepartureTime이 가장 큰 순서대로 정렬되어 있으므로, dispatch의 인덱스가 작을수록 최적인 차량 (가장 최적은 dispatch[0])
 
 const logger = require("../../config/logger");
 const pool2 = require("../util/mysql2");
@@ -12,14 +12,24 @@ const resultDispatch = async (dispatchResult, revData, dire, is3) => {
   let result = undefined;
   const connection = await pool2.getConnection(async (conn) => conn);
   try {
-    const car_id = dispatchResult.dispatch[0].car_id;
+    let car_id;
+    let manager_num;
 
     // 매니저 매칭 - 기본적으로 차량의 운전자로 배정
-    const sqlm =
-      "select M.`netsmanager_number` from `car` as C, `netsmanager` as M where C.`car_id`=? and C.`netsmanager_number`=M.`netsmanager_number`;";
-    const sqlmd = await connection.query(sqlm, [car_id]);
-    if (sqlmd[0].length == 0) throw (err = "매니저 매칭 실패");
-
+    for(let i = 0; i < dispatchResult.dispatch.length; i++)
+    {
+      car_id = dispatchResult.dispatch[i].car_id;
+      const sqlm =
+      "select M.`netsmanager_number` from `car` as C, `netsmanager` as M where C.`car_id`=? and C.`netsmanager_number`=M.`netsmanager_number` and M.`netsmanager_possible`!=0;";
+      const sqlmd = await connection.query(sqlm, [car_id]);
+      if (sqlmd[0].length != 0)
+      {
+        manager_num = sqlmd[0][0].netsmanager_number;
+        break;
+      }
+    }
+    if(manager_num === undefined) throw err = "매니저 매칭 실패!";
+    
     let time_start = dispatchResult.expect_pickup_time;
     let time_end = dispatchResult.expect_terminate_service_time;
     let expect_move_distance = dispatchResult.expect_move_distance;
@@ -38,7 +48,7 @@ const resultDispatch = async (dispatchResult, revData, dire, is3) => {
     time_end = time_end.substr(0, 10) + " " + time_end.substr(11, 8);
 
     result = {
-      netsmanagerNum: sqlmd[0][0].netsmanager_number,
+      netsmanagerNum: manager_num,
       carId: car_id,
       expMoveDistance: expect_move_distance,
       expMoveTime: expect_move_time,
