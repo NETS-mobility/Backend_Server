46838d8e304d1f465b19e3b1ce0c9b9fa066e6fa
jihong <ljihong0526@gmail.com>
2022-04-04 17:50:23 +0900

    add alarm function test

--- server/modules/setting_alarm.js
+++ server/modules/setting_alarm.js
@@ -120,19 +120,19 @@ async function set_alarm(reciever, reservation_id, alarm_kind, user_id, temp) {
           let pickup_time = res[1];
 
           alarm.set_context(
-            "네츠서비스가 매칭되었습니다. \n" +
-              "예약 확정을 위해 결제 부탁드립니다.\n" +
-              "1시간 이내에 결제되지 않을 경우 예약이 취소될 수 있습니다. \n" +
+            "네츠서비스가 매칭되었습니다. " +
+              "예약 확정을 위해 결제 부탁드립니다. " +
+              "1시간 이내에 결제되지 않을 경우 예약이 취소될 수 있습니다. " +
               "서비스번호: " +
               reservation_id +
-              "\n예약일정:" +
+              " 예약일정:" +
               reservation_date +
-              "\n픽업 예정시간: " +
+              " 픽업 예정시간: " +
               pickup_time
           );
           alarm.set_push(
-            "매칭 완료\n",
-            "예약 확정을 위해 결제 부탁드립니다.\n" +
+            "매칭 완료 ",
+            "예약 확정을 위해 결제 부탁드립니다. " +
               "1시간 이내에 결제되지 않을 경우 예약이 취소될 수 있습니다. "
           );
 
@@ -183,20 +183,20 @@ async function set_alarm(reciever, reservation_id, alarm_kind, user_id, temp) {
           logger.info("2번 알림 작동");
 
           alarm.set_context(
-            "네츠서비스가 매칭되었습니다.\n" +
-              "예약 확정을 위해 결제 부탁드립니다.\n" +
-              "30분 이내에 결제되지 않을 경우 예약이 취소될 수 있습니다. \n" +
+            "네츠서비스가 매칭되었습니다. " +
+              "예약 확정을 위해 결제 부탁드립니다. " +
+              "30분 이내에 결제되지 않을 경우 예약이 취소될 수 있습니다.  " +
               "서비스번호: " +
               reservation_id +
-              "\n예약일정: " +
+              " 예약일정: " +
               reservation_date +
-              "\n픽업 예정시간: " +
+              " 픽업 예정시간: " +
               pickup_time
           );
           alarm.set_push(
             "결제 요청",
             "예약 확정을 위해 결제 부탁드립니다." +
-              "\n30분 이내에 결제되지 않을 경우 예약이 취소될 수 있습니다. "
+              " 30분 이내에 결제되지 않을 경우 예약이 취소될 수 있습니다. "
           );
           // 다음 결제 요청 설정
           setTimeout(
@@ -229,18 +229,18 @@ async function set_alarm(reciever, reservation_id, alarm_kind, user_id, temp) {
       case alarm_kind_number.cancellation:
         {
           alarm.set_context(
-            "결제시간 초과로 예약이 취소되었습니다.\n" +
+            "결제시간 초과로 예약이 취소되었습니다. " +
               "서비스번호: " +
               reservation_id +
-              "\n예약일정: " +
+              " 예약일정: " +
               alarm.reservation_date +
-              "\n픽업 예정시간: " +
+              " 픽업 예정시간: " +
               alarm.pickup_time +
-              "\n고객님의 쾌유와 가족의 건강을 기원합니다."
+              " 고객님의 쾌유와 가족의 건강을 기원합니다."
           );
           alarm.set_push(
-            "예약 취소\n",
-            "예약 확정을 위해 결제 부탁드립니다.\n" +
+            "예약 취소 ",
+            "예약 확정을 위해 결제 부탁드립니다. " +
               "결제시간 초과로 예약이 취소되었습니다."
           );
         }
@@ -263,19 +263,19 @@ async function set_alarm(reciever, reservation_id, alarm_kind, user_id, temp) {
           netsmanager_name = res[1];
 
           alarm.set_context(
-            "네츠 예약이 확정되었습니다.\n" +
+            "네츠 예약이 확정되었습니다. " +
               "서비스번호: " +
               reservation_id +
-              "\n예약일정: " +
+              " 예약일정: " +
               alarm.reservation_date +
-              "\n픽업 예정시간: " +
+              " 픽업 예정시간: " +
               alarm.pickup_time +
-              "\n배차 차량번호: " +
+              " 배차 차량번호: " +
               car_id +
-              "\n네츠 매니저: " +
+              " 네츠 매니저: " +
               netsmanager_name +
-              "\n네츠 매니저가 예약 확인을 위해 전화드릴 예정입니다.\n" +
-              "예약 확정 후, 코로나 의심 증상이 있거나 확진자 접촉시 고객센터로 연락해주시기 바랍니다.\n" +
+              " 네츠 매니저가 예약 확인을 위해 전화드릴 예정입니다. " +
+              "예약 확정 후, 코로나 의심 증상이 있거나 확진자 접촉시 고객센터로 연락해주시기 바랍니다. " +
               "고객님의 쾌유를 기원합니다."
           );
 
@@ -315,17 +315,17 @@ async function set_alarm(reciever, reservation_id, alarm_kind, user_id, temp) {
           alarm.set_context(
             "네츠매니저 " +
               netsmanager_name +
-              "입니다.\n" +
+              "입니다. " +
               "금일 예약하신 서비스를 위해 " +
               temp + // temp는 방문 예정시간을 인자로 입력받음
-              "에 방문드릴 예정입니다.\n" +
+              "에 방문드릴 예정입니다. " +
               "감사합니다."
           );
           alarm.set_push(
-            "방문 알림\n",
+            "방문 알림 ",
             "금일 예약하신 서비스를 위해 " +
               temp +
-              "방문드릴 예정입니다.\n" +
+              "방문드릴 예정입니다. " +
               "감사합니다."
           );
         }
@@ -346,16 +346,16 @@ async function set_alarm(reciever, reservation_id, alarm_kind, user_id, temp) {
           alarm.set_context(
             "네츠매니저 " +
               netsmanager_name +
-              "입니다.\n" +
+              "입니다. " +
               "교통체증 등으로 인해 안내드린 픽업시간에서 " +
               temp + // 입력된 delay_time
-              "지연이 예상됩니다.\n 불편을 드린 점, 양해바랍니다."
+              "지연이 예상됩니다.  불편을 드린 점, 양해바랍니다."
           );
           alarm.set_push(
-            "지연 알림\n",
+            "지연 알림 ",
             "교통체증 등으로 인해 안내드린 픽업시간에서 " +
               temp + // 입력된 delay_time
-              "지연이 예상됩니다.\n 불편을 드린 점, 양해바랍니다.\n"
+              "지연이 예상됩니다.  불편을 드린 점, 양해바랍니다. "
           );
         }
         break;
@@ -385,20 +385,20 @@ async function set_alarm(reciever, reservation_id, alarm_kind, user_id, temp) {
             netsmanager_name = res[4];
 
             alarm.set_context(
-              "네츠 차량 픽업이 20분이상 지연되었습니다.\n" +
+              "네츠 차량 픽업이 20분이상 지연되었습니다. " +
                 "서비스번호: " +
                 reservation_id +
-                "\n픽업 시간: " +
+                " 픽업 시간: " +
                 alarm.pickup_time +
-                "\n고객 성함: " +
+                " 고객 성함: " +
                 patient_name +
-                "\n고객 전화: " +
+                " 고객 전화: " +
                 patient_phone +
-                "\n보호자 성함: " +
+                " 보호자 성함: " +
                 protector_name +
-                "\n보호자 전화: " +
+                " 보호자 전화: " +
                 protector_phone +
-                "\n네츠 매니저: " +
+                " 네츠 매니저: " +
                 netsmanager_name
             );
           } catch (err) {
@@ -432,7 +432,7 @@ async function set_alarm(reciever, reservation_id, alarm_kind, user_id, temp) {
 
           alarm.set_context(
             user_name +
-              "고객님 동행 상황 보고\n" +
+              "고객님 동행 상황 보고 " +
               "네츠 서비스 내용: " +
               fixed_medical_detail + // 서비스 내용과 첨부하는 사진은 수정? 가능해야한다.
               ", picture: " +
@@ -458,15 +458,15 @@ async function set_alarm(reciever, reservation_id, alarm_kind, user_id, temp) {
 
           alarm.set_context(
             user_name +
-              "고객님 동행 상황 보고\n" +
+              "고객님 동행 상황 보고 " +
               "네츠 서비스 내용" +
               fixed_medical_detail + // 서비스 내용을 타입으로 해도 되는지?
-              "\n첨부된 사진" +
+              " 첨부된 사진" +
               accompany_picture_path + // TODO: 사진 연결
-              "\n[오늘 동행은 어떠셨을까요?]\n" +
-              "서비스 품질 개선을 위해 고객님의 의견을 듣고자 합니다.\n" +
-              "잠시 시간을 허락하시어 설문해 주신다면 편안하고 안전한 동행을 위해 더 나은 서비스로 노력하겠습니다.\n" +
-              "고객님의 쾌유와 가족의 건강을 기원합니다.\n" +
+              " [오늘 동행은 어떠셨을까요?] " +
+              "서비스 품질 개선을 위해 고객님의 의견을 듣고자 합니다. " +
+              "잠시 시간을 허락하시어 설문해 주신다면 편안하고 안전한 동행을 위해 더 나은 서비스로 노력하겠습니다. " +
+              "고객님의 쾌유와 가족의 건강을 기원합니다. " +
               "네츠 고객 감동실 드림" +
               "설문조사 링크: " +
               url.servey_url
@@ -490,16 +490,16 @@ async function set_alarm(reciever, reservation_id, alarm_kind, user_id, temp) {
           over_cost = temp[1];
 
           alarm.set_context(
-            "병원 동행 서비스 시간이 초과되어 추가요금 결제 부탁드립니다.\n" +
+            "병원 동행 서비스 시간이 초과되어 추가요금 결제 부탁드립니다. " +
               "서비스번호: " +
               reservation_id +
-              "\n최초 예약시간: " +
+              " 최초 예약시간: " +
               origin_service_time +
-              "\n실제 서비스 시간: " +
+              " 실제 서비스 시간: " +
               real_service_time + // 실제 서비스 시간은 입력받음
-              "\n초과 시간: " +
+              " 초과 시간: " +
               over_time +
-              "\n초과 요금: " +
+              " 초과 요금: " +
               over_cost
           );
           alarm.set_push(
@@ -514,12 +514,12 @@ async function set_alarm(reciever, reservation_id, alarm_kind, user_id, temp) {
           wait_time = temp[0];
           wait_cost = temp[1];
           alarm.set_context(
-            "대기요금이 발생하여 결제 부탁드립니다.\n" +
+            "대기요금이 발생하여 결제 부탁드립니다. " +
               "서비스번호: " +
               reservation_id +
-              "\n대기 시간: " +
+              " 대기 시간: " +
               wait_time +
-              "\n대기 요금: " +
+              " 대기 요금: " +
               wait_cost
           );
           alarm.set_push(
@@ -563,20 +563,20 @@ async function set_alarm(reciever, reservation_id, alarm_kind, user_id, temp) {
             console.log(hour);
 
             alarm.set_context(
-              "네츠 예약이 확정되었습니다.\n" +
+              "네츠 예약이 확정되었습니다. " +
                 "서비스번호: " +
                 reservation_id +
-                "\n예약일정: " +
+                " 예약일정: " +
                 alarm.get_reservDate() +
-                "\n픽업 예정시간: " +
+                " 픽업 예정시간: " +
                 alarm.get_pickupTime() +
-                "\n픽업주소: " +
+                " 픽업주소: " +
                 alarm.pickup_address +
-                "\n고객 이름: " +
+                " 고객 이름: " +
                 customer_name +
-                "\n네츠 매니저가 예약 확인을 위해 전화드릴 예정입니다.\n" +
-                "예약 확정 후, 코로나 의심 증상이 있거나 확진자 접촉 시 고객센터로 연락해주시기 바랍니다.\n" +
-                "고객님의 쾌유를 기원합니다.\n"
+                " 네츠 매니저가 예약 확인을 위해 전화드릴 예정입니다. " +
+                "예약 확정 후, 코로나 의심 증상이 있거나 확진자 접촉 시 고객센터로 연락해주시기 바랍니다. " +
+                "고객님의 쾌유를 기원합니다. "
             );
 
             // 하루 전 알림 설정
@@ -612,14 +612,14 @@ async function set_alarm(reciever, reservation_id, alarm_kind, user_id, temp) {
           alarm.set_context(
             "네츠서비스가 내일(" +
               alarm.get_reservDate() +
-              ") 진행됩니다.\n" +
-              "고객님께 해피콜을 진행해주세요.\n" +
-              "차고지(기타장소) 출발시간: \n" +
+              ") 진행됩니다. " +
+              "고객님께 해피콜을 진행해주세요. " +
+              "차고지(기타장소) 출발시간:  " +
               "픽업 예정시간: " +
               alarm.get_pickupTime() +
-              "\n픽업주소: " +
+              " 픽업주소: " +
               pickup_address +
-              "\n고객이름: " +
+              " 고객이름: " +
               customer_name
           );
           alarm.set_push(
