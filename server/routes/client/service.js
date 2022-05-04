844a93c369f873f3a54a865e9b1f08feaf6019e8
n3wzd <87504447+n3wzd@users.noreply.github.com>
2022-05-04 22:35:34 +0900

    client service update
    manager, admin의 service_state 변경 사항을 client에도 적용

--- server/routes/client/service.js
+++ server/routes/client/service.js
@@ -11,6 +11,7 @@ const payment_state = require("../../config/payment_state");
 const rev_state_msg = require("../../modules/reservation_state_msg");
 const case_finder = require("../../modules/dispatch_case_finder");
 const gowith_finder = require("../../modules/dispatch_isOverPoint_finder");
+const get_service_progress = require("../../modules/get_service_progress");
 const logger = require("../../config/logger");
 
 // ===== 서비스 목록 조회 =====
@@ -105,30 +106,8 @@ router.post("/serviceDetail/:service_id", async function (req, res, next) {
     if (data_service.length == 0) throw (err = 0);
 
     // 서비스 상태정보
-    const sql_prog =
-      "select date_format(`real_car_departure_time`,'%Y-%m-%d %T') as `real_car_departure_time`, date_format(`real_pickup_time`,'%Y-%m-%d %T') as `real_pickup_time`, date_format(`real_hospital_arrival_time`,'%Y-%m-%d %T') as `real_hospital_arrival_time`, date_format(`real_return_hospital_arrival_time`,'%Y-%m-%d %T') as `real_return_hospital_arrival_time`, " + 
-      "date_format(`real_return_start_time`,'%Y-%m-%d %T') as `real_return_start_time`, date_format(`real_service_end_time`,'%Y-%m-%d %T') as `real_service_end_time`, `service_state_id` from `service_progress` where `reservation_id`=?;";
-    const result_prog = await connection.query(sql_prog, [service_id]);
-    const data_prog = result_prog[0];
-
-    let sstate = 0;
-    let sstate_time = undefined;
-    if (data_prog.length > 0) {
-      sstate = data_prog[0].service_state_id;
-      sstate_time = [ 0 ];
-      if(data_service[0].move_direction_id != 2)
-      {
-        sstate_time.push(data_prog[0].real_car_departure_time); // 차량출발
-        sstate_time.push(data_prog[0].real_pickup_time); // 픽업완료
-        sstate_time.push(data_prog[0].real_hospital_arrival_time); // 병원도착
-      }
-      if(data_service[0].move_direction_id != 1)
-      {
-        sstate_time.push(data_prog[0].real_return_hospital_arrival_time); // 귀가차량 병원도착
-        sstate_time.push(data_prog[0].real_return_start_time); // 귀가출발
-        sstate_time.push(data_prog[0].real_service_end_time); // 서비스종료
-      }
-    }
+    const service_prog = await get_service_progress(service_id, data_service[0].move_direction_id);
+    if(service_prog === undefined) throw (err = 0);
 
     // 매니저 정보
     const sqld =
@@ -183,8 +162,8 @@ router.post("/serviceDetail/:service_id", async function (req, res, next) {
     res.send({
       dispatch: sqldd,
       service: data_service[0],
-      service_state: sstate,
-      service_state_time: sstate_time,
+      service_state: service_prog.service_state,
+      service_state_time: service_prog.service_state_time,
       payment: {
         charge: charge,
         extraPay: extraPay,
